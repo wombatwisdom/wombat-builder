c711779bcbc588e41c5d9e89e468703afb458d2a
Daan Gerits <daan.gerits@gmail.com>
2024-06-06 16:47:45 +0200

    changed the title and favicon
    Signed-off-by: Daan Gerits <daan.gerits@gmail.com>

--- internal/api/web/src/layouts/default.vue
+++ internal/api/web/src/layouts/default.vue
@@ -4,6 +4,17 @@
       <v-spacer></v-spacer>
       <v-btn text="Home" to="/" variant="plain" slim color="primary" />
       <v-btn text="About" to="/about" variant="text" slim/>
+      <a
+        href="https://github.com/wombatwisdom/wombat-builder"
+        title="Wombat Builder"
+        class="d-inline-block mx-2 mr-6 social-link"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        <v-icon
+          icon="mdi-github"
+        />
+      </a>
     </v-app-bar>
     <v-main>
       <router-view />
